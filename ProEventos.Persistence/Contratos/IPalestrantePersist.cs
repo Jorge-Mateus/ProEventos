9845f687b7885ea6d90f83af5b94d4d6aa5cd4d1
Jorge Mateus <69761413+Jorge-Mateus@users.noreply.github.com>
2025-02-27 18:51:16 -0300

    Organizando o projeto com atualizacoes de arquitetura, solid, context e mais

---
+++ ProEventos.Persistence/Contratos/IPalestrantePersist.cs
@@ -0,0 +1,12 @@
+﻿using ProEventos.Domain.Models;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IPalestrantePersist
+    {
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string tema, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+    }
+}
