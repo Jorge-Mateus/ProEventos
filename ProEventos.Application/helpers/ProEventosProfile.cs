128a23a5d7f8d1e50a9ae538d85da250eba1aa63
Jorge Mateus <69761413+Jorge-Mateus@users.noreply.github.com>
2025-03-01 19:03:27 -0300

    dtos,mapeacao, validacao de campo

---
+++ ProEventos.Application/helpers/ProEventosProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProEventos.Application.DTOs;
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Application.helpers
+{
+    public class ProEventosProfile : Profile 
+    {
+        public ProEventosProfile()
+        {
+            CreateMap<Evento, EventoDto>().ReverseMap();
+            CreateMap<Lote, LoteDto>().ReverseMap();
+            CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+            CreateMap<Palestrante, PalestranteDto>().ReverseMap();
+
+        }
+    }
+}
